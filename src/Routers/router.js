b3b76fb7703b35125f75146241fc7787d8164df9
AnasTomeh <eng.anas.tomeh@gmail.com>
2023-11-01 01:56:30 +0200

    Creating About Page

--- src/Routers/router.js
+++ src/Routers/router.js
@@ -1,6 +1,7 @@
 import {createBrowserRouter} from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
 import Layout from "../Layouts/Layout";
+import AboutPage from "../pages/AboutPage";
 
 
 export const router = createBrowserRouter([
@@ -11,6 +12,10 @@ export const router = createBrowserRouter([
             {
                 path: "/",
                 element: <LandingPage />,
+            },
+            {
+                path: "/about-page",
+                element: <AboutPage />,
             },
         ]
     }
