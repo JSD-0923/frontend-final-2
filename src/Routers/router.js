d416e5d2edce9347aacd1910712246cbcbf1f1c2
AnasTomeh <eng.anas.tomeh@gmail.com>
2023-11-01 19:08:33 +0200

    Modifying My Orders Page: 1- Remove My Orders Container Component 2- Update Component Structure

--- src/Routers/router.js
+++ src/Routers/router.js
@@ -1,7 +1,6 @@
 import {createBrowserRouter} from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
 import Layout from "../Layouts/Layout";
-import MyOrdersPage from "../pages/MyOrdersPage";
 
 
 export const router = createBrowserRouter([
@@ -12,10 +11,6 @@ export const router = createBrowserRouter([
             {
                 path: "/",
                 element: <LandingPage />,
-            },
-            {
-                path: "/user-profile/my-orders",
-                element: <MyOrdersPage />,
             },
         ]
     }
