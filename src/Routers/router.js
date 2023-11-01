ae7f052b72883162b9d93406b2f40c2485b5ee93
AnasTomeh <eng.anas.tomeh@gmail.com>
2023-11-01 19:04:16 +0200

    Modifying Checkout Page Payments Page: 1- Remove Checkout Page Payments Component 2- Update Component Structure

--- src/Routers/router.js
+++ src/Routers/router.js
@@ -1,7 +1,6 @@
 import {createBrowserRouter} from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
 import Layout from "../Layouts/Layout";
-import CheckoutPagePayment from "../pages/CheckoutPagePayment";
 
 
 export const router = createBrowserRouter([
@@ -12,10 +11,6 @@ export const router = createBrowserRouter([
             {
                 path: "/",
                 element: <LandingPage />,
-            },
-            {
-                path: "/checkout-page-payment",
-                element: <CheckoutPagePayment />,
             },
         ]
     }
--- src/pages/CheckoutPagePayment.js
+++ src/pages/CheckoutPagePayment.js
@@ -1,10 +1,20 @@
-import CheckoutPagePaymentContainer from "../components/CheckoutPagePaymentContainer/CheckoutPagePaymentContainer";
+import ContactInformation from "../components/ContactInformation/ContactInformation";
+import PaymentMethods from "../components/PaymentMethods/PaymentMethods";
+import PaymentMethodDetails from "../components/PaymentMethodDetails/PaymentMethodDetails";
 
 
 const CheckoutPagePayment = () => {
 
     return (
-        <CheckoutPagePaymentContainer />
+        <div>
+            <h2>this is breadcrumbs Component</h2>
+            <h2>This is Checkout page Title</h2>
+            <ContactInformation/>
+            <PaymentMethods />
+            <PaymentMethodDetails />
+            <h4>Back to Cart Button</h4>
+            <h4>Next Button</h4>
+        </div>
     )
 }
 
