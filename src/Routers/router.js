849c163516f1cd3e474c946f12369a2e8c50f3ed
AnasTomeh <eng.anas.tomeh@gmail.com>
2023-11-01 19:15:08 +0200

    Modifying User Profile Page: 1- Remove User Profile Container Component 2- Update Component Structure

--- src/Routers/router.js
+++ src/Routers/router.js
@@ -1,7 +1,6 @@
 import {createBrowserRouter} from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
 import Layout from "../Layouts/Layout";
-import UserProfile from "../pages/UserProfile";
 
 
 export const router = createBrowserRouter([
@@ -12,10 +11,6 @@ export const router = createBrowserRouter([
             {
                 path: "/",
                 element: <LandingPage />,
-            },
-            {
-                path: "/user-profile",
-                element: <UserProfile />,
             },
         ]
     }
--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,10 +1,20 @@
-import UserProfileContainer from "../components/UserProfileContainer/UserProfileContainer";
+import UserProfileMenu from "../components/UserProfileMenu/UserProfileMenu";
+import PersonalInformation from "../components/PersonalInformation/PersonalInformation";
+import ChangePasswordForm from "../components/ChangePasswordForm/ChangePasswordForm";
 
 
 const UserProfile = () => {
 
     return (
-        <UserProfileContainer />
+        <div>
+            <h3>this is breadcrumbs Component</h3>
+            <h3>This is Personal Information Title</h3>
+            <h3>logout button</h3>
+            <UserProfileMenu/>
+            <PersonalInformation />
+            <ChangePasswordForm />
+            <h3>Save changes button</h3>
+        </div>
     )
 }
 
