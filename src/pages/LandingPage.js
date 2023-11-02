39de7727c50df6fb918183b7a86a9e93d1499385
Noura-Darwazeh <noura.darwazeh11@gmail.com>
2023-11-01 20:35:39 -0500

    Components in landing  page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,19 @@
-import {Typography} from "@mui/material";
-import LandingPageContainer from '../components/LandingPageContainer/LandingPageContainer'
+import { Typography } from "@mui/material";
+import HeroSection from '../components/HeroSection/HeroSection'
+import NewArrival from '../components/NewArrival/NewArrival'
+import HandpickedCollections from '../components/HandpickedCollections/HandpickedCollections'
+import Brands from '../components/Brands/Brands'
+import FilterCards from '../components/FilterCards/FilterCards'
 const LandingPage = () => {
 
     return (
         <div>
-            <Typography  variant='displayLarge' component={'h2'}>Landing Page</Typography>
-            <LandingPageContainer />
+            <Typography variant='displayLarge' component={'h2'}>Landing Page</Typography>
+            <HeroSection />
+            <NewArrival />
+            <HandpickedCollections />
+            <Brands />
+            <FilterCards />
         </div>
     )
 }
