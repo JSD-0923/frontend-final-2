42f79f2f54143d755c5cc7b26fafa1c69a9de543
AnasTomeh <eng.anas.tomeh@gmail.com>
2023-11-05 11:33:36 +0200

    1- Create Handpicked Collections Section 2- Create Shop by Brands 3- Create filtered Section

--- src/components/HandpickedCard/HandpickedCard.js
+++ src/components/HandpickedCard/HandpickedCard.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import {CardMedia} from "@mui/material";
+import {HandPickedBox, HandPickedCard, HandPickedTypographyH2} from "./style";
 
-const HandpickedCard = () => {
-  
+const HandpickedCard = (props) => {
+
+    const {collection} = props
+
+
+    if (!props) {
+        return <></>
+    }
+
   return (
     <div>
-      Handpicked Card
+            <HandPickedCard>
+                <CardMedia
+                    sx={{ height: '100%', width: 'cover' }}
+                    component="img"
+                    image={require(`../../${collection.image}`)}
+                />
+                <HandPickedBox>
+                    <HandPickedTypographyH2 component={"h2"}  >
+                        {collection.title}
+                    </HandPickedTypographyH2>
+                </HandPickedBox>
+            </HandPickedCard>
     </div>
   )
 }
--- src/components/HandpickedCollections/HandpickedCollections.js
+++ src/components/HandpickedCollections/HandpickedCollections.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import HandpickedCard from '../HandpickedCard/HandpickedCard'
+import {Typography} from "@mui/material";
+import {HandpickedCollectionsStyledBox, HandpickedCollectionsStyledPaper} from "./style";
+import {HandpickedCollectionsData} from "./HandpickedCollectionsMock";
 const HandpickedCollections = () => {
-  return (
-    <div>
-      <HandpickedCard/>
-    </div>
+
+
+    return (
+
+      <HandpickedCollectionsStyledPaper >
+          <Typography color={'white'}>Handpicked Collections</Typography>
+         <HandpickedCollectionsStyledBox >
+             {HandpickedCollectionsData.map((item) =>
+                 <HandpickedCard key={item.id} collection={item}/>
+             )}
+
+         </HandpickedCollectionsStyledBox>
+      </HandpickedCollectionsStyledPaper>
   )
 }
 
