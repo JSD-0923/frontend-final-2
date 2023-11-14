c1a79ad194e85ccf8dfaef3300f5933cbf8a1549
AnasTomeh <eng.anas.tomeh@gmail.com>
2023-11-14 02:04:59 +0200

    create sign in aoi integration and useAouth custom hook

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import {responsiveFontSizes, ThemeProvider} from "@mui/material";
 import theme from "./themes/customTheme";
+import {AuthProvider} from "./hooks/useAuth";
 
 let responsiveTheme = responsiveFontSizes(theme);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <ThemeProvider theme={responsiveTheme}>
+        <AuthProvider>
         <App />
+        </AuthProvider>
     </ThemeProvider>
 );
 
