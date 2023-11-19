e97a082f403aa7378af02b7a6c87a130057ef3d2
Noura-Darwazeh <noura.darwazeh11@gmail.com>
2023-11-19 16:36:43 -0500

    edit search

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -2,22 +2,17 @@ import React from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
 
 export const useSearch = (value) => {
-    // برجع السيرتش باراميتر الحالي وبخزنو باللوكيشن
     const navigate = useNavigate()
     let queryString = useLocation().search;
-    
+
     const handleProductsPage = () => {
-        console.log('hi')
-        console.log(queryString)
-        if(queryString)
-        {
+
+        if (queryString) {
             navigate(`products${queryString}&search=${value}`)
         }
-        else{
+        else {
             navigate(`products?search=${value}`)
         }
-        
-        console.log(value)
 
     }
     return { handleProductsPage }
