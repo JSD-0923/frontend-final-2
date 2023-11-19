68cf988dd135a3f1f999d30d54d6ca72b0fa18c7
Noura-Darwazeh <noura.darwazeh11@gmail.com>
2023-11-19 16:27:10 -0500

    edit hero section

--- src/components/data.js
+++ src/components/data.js
@@ -1,31 +1,31 @@
 import hero from '../assets/images/hero.png'
 export const Items = [
     {
-        category: "handbags",
+        id: 1,
         image: hero,
         title: "Carry your Funk",
         subtitle: "Trendy handbags collection for your party animal"
     },
     {
-        category: "watches",
+        id: 2,
         image: hero,
         title: "Carry your Funk",
         subtitle: "Trendy watches collection for your party animal",
     },
     {
-        category: "skincare",
+        id: 3,
         image: hero,
         title: "Carry your Funk",
         subtitle: "Trendy skincare collection for your party animal"
     },
     {
-        category: "jewellery",
+        id: 4,
         image:hero,
         title:"Carry your Funk",
         subtitle: "Trendy jewellery collection for your party animal"
     },
     {
-        category: "Apparels",
+        id: 5,
         image: hero,
         title: "Carry your Funk",
         subtitle: "Trendy Apparels collection for your party animal"
